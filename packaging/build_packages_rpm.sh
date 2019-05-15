05c6b10215e68230aba82f4456404c5d929d06e2
Subrat Prasad <subratp@google.com>
2019-05-15 15:20:07 -0700

    Make git repo pull agnostic to job type

--- packaging/build_packages_rpm.sh
+++ packaging/build_packages_rpm.sh
@@ -32,20 +32,13 @@ export PULL_REF=$(curl -f -H Metadata-Flavor:Google ${URL}/pull-ref)
 # centos6 has some issues with network on first boot
 el6_install(){
   n=0
-  while ! yum install -y https://centos6.iuscommunity.org/ius-release.rpm; do
+  while ! yum install -y https://rhel6.iuscommunity.org/ius-release.rpm; do
     if [[ n -gt 3 ]]; then
       exit 1
     fi
     n=$[$n+1]
     sleep 5
   done
-
-  rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-IUS-6
-}
-
-el7_install(){
-  yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-  rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-IUS-7
 }
 
 # Install git2 as this is not avaiable in centos 6/7
@@ -53,8 +46,9 @@ RELEASE_RPM=$(rpm -qf /etc/redhat-release)
 RELEASE=$(rpm -q --qf '%{VERSION}' ${RELEASE_RPM})
 case ${RELEASE} in
   6*) el6_install;;
-  7*) el7_install;;
+  7*) yum -y install https://rhel7.iuscommunity.org/ius-release.rpm;;
 esac
+rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
 yum install -y git2u
 
 packaging/pull_repository.sh
