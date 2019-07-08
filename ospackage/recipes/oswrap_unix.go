b42b7530a930db44cbc2812a0d0d2d99cc291dfe
Trevor Boone <trevorboone@google.com>
2019-07-08 14:56:43 -0500

    deleting unused oswrap functions

--- ospackage/recipes/oswrap_unix.go
+++ ospackage/recipes/oswrap_unix.go
@@ -18,65 +18,9 @@ package recipes
 
 import (
 	"os"
-	"path/filepath"
 )
-
-// RemoveOnReboot not implemented on non Windows.
-func RemoveOnReboot(name string) error {
-	return nil
-}
-
-// Open calls os.Open
-func Open(name string) (*os.File, error) {
-	return os.Open(name)
-}
 
 // Create calls os.Create
 func Create(name string) (*os.File, error) {
 	return os.Create(name)
-}
-
-// OpenFile calls os.OpenFile
-func OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
-	return os.OpenFile(name, flag, perm)
-}
-
-// Remove calls os.Remove
-func Remove(name string) error {
-	return os.Remove(name)
-}
-
-// RemoveAll calls os.RemoveAll
-func RemoveAll(name string) error {
-	return os.RemoveAll(name)
-}
-
-// Mkdir calls os.Mkdir
-func Mkdir(name string, mode os.FileMode) error {
-	return os.Mkdir(name, mode)
-}
-
-// MkdirAll calls os.MkdirAll
-func MkdirAll(name string, mode os.FileMode) error {
-	return os.MkdirAll(name, mode)
-}
-
-// Rename calls os.Rename
-func Rename(oldpath, newpath string) error {
-	return os.Rename(oldpath, newpath)
-}
-
-// Lstat calls os.Lstat
-func Lstat(name string) (os.FileInfo, error) {
-	return os.Lstat(name)
-}
-
-// Stat calls os.Stat
-func Stat(name string) (os.FileInfo, error) {
-	return os.Stat(name)
-}
-
-// Walk calls filepath.Walk
-func Walk(root string, walkFn filepath.WalkFunc) error {
-	return filepath.Walk(root, walkFn)
 }
--- ospackage/recipes/oswrap_windows.go
+++ ospackage/recipes/oswrap_windows.go
@@ -17,25 +17,9 @@
 package recipes
 
 import (
-	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-	"syscall"
-	"unsafe"
-
-	"golang.org/x/sys/windows"
-)
-
-var (
-	kernel32       = windows.NewLazySystemDLL("Kernel32.dll")
-	procMoveFileEx = kernel32.NewProc("MoveFileExW")
-)
-
-type (
-	DWORD   uint32
-	LPCTSTR *uint16
 )
 
 const MOVEFILE_DELAY_UNTIL_REBOOT = 4
@@ -55,36 +39,6 @@ func normPath(path string) (string, error) {
 	return "\\\\?\\" + path, nil
 }
 
-// RemoveOnReboot schedules a file for removal on next reboot.
-func RemoveOnReboot(name string) error {
-	name, err := normPath(name)
-	if err != nil {
-		return err
-	}
-
-	nPtr, err := syscall.UTF16PtrFromString(name)
-	if err != nil {
-		return fmt.Errorf("error encoding path to UTF16: %v", err)
-	}
-	ret, _, _ := procMoveFileEx.Call(
-		uintptr(unsafe.Pointer(nPtr)),
-		uintptr(0),
-		MOVEFILE_DELAY_UNTIL_REBOOT)
-	if ret == 0 {
-		return errors.New("return code of '0' (failure) from MoveFileEx")
-	}
-	return nil
-}
-
-// Open calls os.Open with name normalized
-func Open(name string) (*os.File, error) {
-	name, err := normPath(name)
-	if err != nil {
-		return nil, err
-	}
-	return os.Open(name)
-}
-
 // Create calls os.Create with name normalized
 func Create(name string) (*os.File, error) {
 	name, err := normPath(name)
@@ -92,124 +46,4 @@ func Create(name string) (*os.File, error) {
 		return nil, err
 	}
 	return os.Create(name)
-}
-
-// OpenFile calls os.OpenFile with name normalized
-func OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
-	name, err := normPath(name)
-	if err != nil {
-		return nil, err
-	}
-	return os.OpenFile(name, flag, perm)
-}
-
-func isProtectedPath(path string) (bool, error) {
-	if path == "" {
-		return false, errors.New("cannot use empty path")
-	}
-	paths := []string{}
-	for _, v := range []string{"SystemDrive", "SystemRoot", "ProgramFiles", "ProgramFiles(x86)"} {
-		paths = append(paths, os.Getenv(v))
-	}
-	paths = append(paths, filepath.Join(os.Getenv("SystemRoot"), "system32"))
-
-	for _, p := range paths {
-		if strings.ToLower(filepath.Clean(p)) == strings.ToLower(filepath.Clean(path)) {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-// Remove calls os.Remove with name normalized
-func Remove(name string) error {
-	if bad, err := isProtectedPath(name); bad || err != nil {
-		return fmt.Errorf("path %s appears to be protected", name)
-	}
-	name, err := normPath(name)
-	if err != nil {
-		return nil
-	}
-	return os.Remove(name)
-}
-
-// RemoveAll calls os.RemoveAll with name normalized
-func RemoveAll(name string) error {
-	if bad, err := isProtectedPath(name); bad || err != nil {
-		return fmt.Errorf("path %s appears to be protected", name)
-	}
-	name, err := normPath(name)
-	if err != nil {
-		return nil
-	}
-	return os.RemoveAll(name)
-}
-
-// Mkdir calls os.Mkdir with name normalized
-func Mkdir(name string, mode os.FileMode) error {
-	name, err := normPath(name)
-	if err != nil {
-		return err
-	}
-	return os.Mkdir(name, mode)
-}
-
-// MkdirAll calls os.MkdirAll with name normalized
-func MkdirAll(name string, mode os.FileMode) error {
-	name, err := normPath(name)
-	if err != nil {
-		return err
-	}
-
-	// os.MkdirAll does not work with extended-length paths if
-	// nothing in the path exists.
-	if err := mkRootDir(name, mode); err != nil {
-		return err
-	}
-
-	return os.MkdirAll(name, mode)
-}
-
-// Rename calls os.Rename with name normalized
-func Rename(oldpath, newpath string) error {
-	oldpath, err := normPath(oldpath)
-	if err != nil {
-		return err
-	}
-	newpath, err = normPath(newpath)
-	if err != nil {
-		return err
-	}
-	return os.Rename(oldpath, newpath)
-}
-
-// Lstat calls os.Lstat with name normalized
-func Lstat(name string) (os.FileInfo, error) {
-	name, err := normPath(name)
-	if err != nil {
-		return nil, err
-	}
-	return os.Lstat(name)
-}
-
-// Stat calls os.Stat with name normalized
-func Stat(name string) (os.FileInfo, error) {
-	name, err := normPath(name)
-	if err != nil {
-		return nil, err
-	}
-	return os.Stat(name)
-}
-
-// Walk calls filepath.Walk with name normalized, and un-normalizes name before
-// calling walkFn
-func Walk(root string, walkFn filepath.WalkFunc) error {
-	newroot, err := normPath(root)
-	if err != nil {
-		return err
-	}
-	return filepath.Walk(newroot, func(path string, info os.FileInfo, err error) error {
-		oldpath := root + strings.TrimPrefix(path, newroot)
-		return walkFn(oldpath, info, err)
-	})
 }
