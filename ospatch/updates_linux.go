0a1fa869dd8ce981e051df2c7a8aa7423e01f03d
Andrew Jackura <ajackura@google.com>
2019-07-31 09:59:01 -0700

    add tests

--- ospatch/updates_linux.go
+++ ospatch/updates_linux.go
@@ -17,14 +17,9 @@
 package ospatch
 
 import (
-	"bufio"
-	"bytes"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"strconv"
 	"strings"
 	"time"
 
@@ -32,96 +27,6 @@ import (
 
 	osconfigpb "github.com/GoogleCloudPlatform/osconfig/_internal/gapi-cloud-osconfig-go/google.golang.org/genproto/googleapis/cloud/osconfig/v1alpha2"
 )
-
-const (
-	systemctl = "/bin/systemctl"
-	reboot    = "/bin/reboot"
-	shutdown  = "/bin/shutdown"
-	rpmQuery  = "/usr/bin/rpmquery"
-)
-
-func exists(path string) (bool, error) {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-		return false, err
-	}
-
-	return true, nil
-}
-
-func getBtime() (int, error) {
-	f, err := os.Open("/proc/stat")
-	if err != nil {
-		return 0, fmt.Errorf("error opening /proc/stat: %v", err)
-	}
-	defer f.Close()
-
-	var btime int
-	scnr := bufio.NewScanner(f)
-	for scnr.Scan() {
-		if bytes.HasPrefix(scnr.Bytes(), []byte("btime")) {
-			split := bytes.SplitN(scnr.Bytes(), []byte(" "), 2)
-			if len(split) != 2 {
-				return 0, fmt.Errorf("error parsing btime from /proc/stat: %q", scnr.Text())
-			}
-			btime, err = strconv.Atoi(string(bytes.TrimSpace(split[1])))
-			if err != nil {
-				return 0, fmt.Errorf("error parsing btime: %v", err)
-			}
-			break
-		}
-	}
-	if err := scnr.Err(); err != nil && btime == 0 {
-		return 0, fmt.Errorf("error scanning /proc/stat: %v", err)
-	}
-	if btime == 0 {
-		return 0, errors.New("could not find btime in /proc/stat")
-	}
-
-	return btime, nil
-}
-
-func rpmReboot() (bool, error) {
-	provides := []string{
-		// Common packages.
-		"kernel", "glibc", "gnutls",
-		// EL packages.
-		"linux-firmware", "openssl-libs", "dbus",
-		// Suse packages.
-		"kernel-firmware", "libopenssl1_1", "libopenssl1_0_0", "dbus-1",
-	}
-	args := append([]string{"--queryformat", `"%{INSTALLTIME}\n"`, "--whatprovides"}, provides...)
-	out, err := exec.Command(rpmQuery, args...).Output()
-	if err != nil {
-		// We don't care about return codes as we know some of these packages won't be installed.
-		if _, ok := err.(*exec.ExitError); !ok {
-			return false, fmt.Errorf("error running %s: %v", rpmQuery, err)
-		}
-	}
-
-	btime, err := getBtime()
-	if err != nil {
-		return false, err
-	}
-
-	// Scanning this output is best effort, false negatives are much prefered
-	// to false positives, and keeping this as simple as possible is
-	// beneficial.
-	scnr := bufio.NewScanner(bytes.NewReader(out))
-	for scnr.Scan() {
-		itime, err := strconv.Atoi(scnr.Text())
-		if err != nil {
-			continue
-		}
-		if itime > btime {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
 
 func (r *patchRun) systemRebootRequired() (bool, error) {
 	if packages.AptExists {
