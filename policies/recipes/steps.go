1bf9c79912da972d51008ed494dd4cc8d1fd11d2
Liam Hopkins <liamh@google.com>
2019-08-09 11:25:54 -0700

    Interpreter changes; support allowedExitCodes

--- policies/recipes/steps.go
+++ policies/recipes/steps.go
@@ -22,7 +22,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strconv"
-	"strings"
 
 	osconfigpb "github.com/GoogleCloudPlatform/osconfig/_internal/gapi-cloud-osconfig-go/google.golang.org/genproto/googleapis/cloud/osconfig/v1alpha2"
 )
@@ -127,106 +126,76 @@ func StepFileExec(step *osconfigpb.SoftwareRecipe_Step_FileExec, artifacts map[s
 		return fmt.Errorf("can't determine location type")
 	}
 
-	return executeCommand(path, stepDir, runEnvs, step.FileExec.Args...)
+	return executeCommand(path, step.FileExec.Args, stepDir, runEnvs, []int32{0})
 }
 
-// StepScriptRun builds the command for a ScriptRun step
+// StepScriptRun runs a ScriptRun step.
 func StepScriptRun(step *osconfigpb.SoftwareRecipe_Step_ScriptRun, artifacts map[string]string, runEnvs []string, stepDir string) error {
+	cmd := filepath.Join(stepDir, "recipe_script_source")
+	if err := writeScript(cmd, step.ScriptRun.Script); err != nil {
+		return err
+	}
+
+	var args []string
 	switch step.ScriptRun.Interpreter {
 	case osconfigpb.SoftwareRecipe_Step_RunScript_INTERPRETER_UNSPECIFIED:
-		return scriptRunDirect(step, runEnvs, stepDir)
+		if runtime.GOOS == "windows" {
+			args = []string{"/c", cmd}
+			cmd = "C:\\Windows\\System32\\cmd.exe"
+		}
 	case osconfigpb.SoftwareRecipe_Step_RunScript_SHELL:
 		if runtime.GOOS == "windows" {
-			return scriptRunCmd(step, runEnvs, stepDir)
+			args = []string{"/c", cmd}
+			cmd = "C:\\Windows\\System32\\cmd.exe"
 		}
-		return scriptRunSh(step, runEnvs, stepDir)
+		args = []string{"-c", cmd}
+		cmd = "/bin/sh"
 	case osconfigpb.SoftwareRecipe_Step_RunScript_POWERSHELL:
-		if runtime.GOOS == "windows" {
-			return scriptRunPowershell(step, runEnvs, stepDir)
+		if runtime.GOOS != "windows" {
+			return fmt.Errorf("interpreter %q can only used on Windows systems", step.ScriptRun.Interpreter)
 		}
-		return fmt.Errorf("interpreter %q cannot be used on non-Windows system", step.ScriptRun.Interpreter)
+		args = []string{"-File", cmd}
+		cmd = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\PowerShell.exe"
 	default:
-		return fmt.Errorf("invalid interpreter %q", step.ScriptRun.Interpreter)
+		return fmt.Errorf("unsupported interpreter %q", step.ScriptRun.Interpreter)
 	}
+	return executeCommand(cmd, args, stepDir, runEnvs, step.ScriptRun.AllowedExitCodes)
 }
 
-func scriptRunSh(step *osconfigpb.SoftwareRecipe_Step_ScriptRun, runEnvs []string, stepDir string) error {
-	scriptPath := filepath.Join(stepDir, "script")
-
-	if err := writeScript(scriptPath, step.ScriptRun.Script); err != nil {
-		return nil
-	}
-
-	var qargs []string
-	for _, arg := range step.ScriptRun.Args {
-		qargs = append(qargs, fmt.Sprintf("%q", arg))
-	}
-	command := scriptPath + " " + strings.Join(qargs, " ")
-	args := []string{"-c", command}
-	return executeCommand("/bin/sh", stepDir, runEnvs, args...)
-}
-
-func scriptRunDirect(step *osconfigpb.SoftwareRecipe_Step_ScriptRun, runEnvs []string, stepDir string) error {
-	scriptPath := filepath.Join(stepDir, "script")
-
-	if err := writeScript(scriptPath, step.ScriptRun.Script); err != nil {
-		return err
-	}
-
-	return executeCommand(scriptPath, stepDir, runEnvs, step.ScriptRun.Args...)
-}
-
-func scriptRunPowershell(step *osconfigpb.SoftwareRecipe_Step_ScriptRun, runEnvs []string, stepDir string) error {
-	scriptPath := filepath.Join(stepDir, "script.ps1")
-
-	if err := writeScript(scriptPath, step.ScriptRun.Script); err != nil {
-		return err
-	}
-
-	args := append([]string{"-File", scriptPath}, step.ScriptRun.Args...)
-	return executeCommand("PowerShell", stepDir, runEnvs, args...)
-}
-
-func scriptRunCmd(step *osconfigpb.SoftwareRecipe_Step_ScriptRun, runEnvs []string, stepDir string) error {
-	scriptPath := filepath.Join(stepDir, "script.bat")
-
-	if err := writeScript(scriptPath, step.ScriptRun.Script); err != nil {
-		return err
-	}
-
-	var qargs []string
-	for _, arg := range step.ScriptRun.Args {
-		qargs = append(qargs, fmt.Sprintf("%q", arg))
-	}
-	command := scriptPath + " " + strings.Join(qargs, " ")
-	args := []string{"/c", command}
-	return executeCommand("cmd", stepDir, runEnvs, args...)
-}
-
-func writeScript(path, script string) error {
+func writeScript(path, contents string) error {
 	f, err := os.Create(path)
 	if err != nil {
 		return err
 	}
-
-	f.WriteString(script)
-	f.Close()
-	if err := os.Chmod(path, 0755); err != nil {
+	f.WriteString(contents)
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+	err = os.Chmod(path, 0755)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func executeCommand(cmd string, workDir string, runEnvs []string, args ...string) error {
+func executeCommand(cmd string, args []string, workDir string, runEnvs []string, allowedExitCodes []int32) error {
 	cmdObj := exec.Command(cmd, args...)
-
 	cmdObj.Dir = workDir
 	cmdObj.Env = append(cmdObj.Env, runEnvs...)
 
 	// TODO: log output from command.
 	_, err := cmdObj.Output()
-	if err != nil {
-		return err
+	if err == nil {
+		return nil
 	}
-	return nil
+	if v, ok := err.(*exec.ExitError); ok && len(allowedExitCodes) != 0 {
+		result := int32(v.ExitCode())
+		for _, code := range allowedExitCodes {
+			if result == code {
+				return nil
+			}
+		}
+	}
+	return err
 }
