b18362c3fff3ff51a0714ea3152c0e2b02677d17
Subrat Prasad <subratp@google.com>
2019-09-09 13:51:54 -0700

    fix linter

--- util/util.go
+++ util/util.go
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-// Package common contains common functions for use in the osconfig agent.
+// Package util contains common functions for use in the osconfig agent.
 package util
 
 import (
