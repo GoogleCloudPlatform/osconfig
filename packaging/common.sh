54d916327415ee57107275d2978ce4acd1312711
Liam Hopkins <liamh@google.com>
2019-09-10 11:08:36 -0700

    no upgrade in deb build

--- packaging/common.sh
+++ packaging/common.sh
@@ -16,7 +16,7 @@
 set -e
 
 export PKGNAME="google-osconfig-agent"
-export VERSION="20190819.00"
+export VERSION="20190910.00"
 
 function exit_error() {
   echo "build failed: $0:$1 \"$BASH_COMMAND\" returned $?"
