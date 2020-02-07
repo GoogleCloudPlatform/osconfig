df8742b623cc3b6c341390f7970c93567a98a1dc
Andrew Jackura <ajackura@google.com>
2020-02-07 15:27:43 -0800

    update

--- e2e_tests/gcp_clients/gcp_clients.go
+++ e2e_tests/gcp_clients/gcp_clients.go
@@ -19,14 +19,12 @@ import (
 	"fmt"
 
 	osconfigV1beta "cloud.google.com/go/osconfig/apiv1beta"
-	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/compute-image-tools/daisy/compute"
 	"github.com/GoogleCloudPlatform/osconfig/e2e_tests/config"
 	"google.golang.org/api/option"
 )
 
 var (
-	storageClient        *storage.Client
 	computeClient        compute.Client
 	osconfigClientV1beta *osconfigV1beta.Client
 )
@@ -48,12 +46,6 @@ func createComputeClient(ctx context.Context) error {
 	return err
 }
 
-func createStorageClient(ctx context.Context) error {
-	var err error
-	storageClient, err = storage.NewClient(ctx, option.WithCredentialsFile(config.OauthPath()))
-	return err
-}
-
 func createOsConfigClientV1beta(ctx context.Context) error {
 	var err error
 	osconfigClientV1beta, err = osconfigV1beta.NewClient(ctx, option.WithCredentialsFile(config.OauthPath()), option.WithEndpoint(config.SvcEndpoint()))
@@ -66,14 +58,6 @@ func GetComputeClient() (compute.Client, error) {
 		return nil, fmt.Errorf("compute client was not initialized")
 	}
 	return computeClient, nil
-}
-
-// GetStorageClient returns a singleton GCP client for osconfig tests
-func GetStorageClient() (*storage.Client, error) {
-	if storageClient == nil {
-		return nil, fmt.Errorf("storage client was not initialized")
-	}
-	return storageClient, nil
 }
 
 // GetOsConfigClientV1beta returns a singleton GCP client for osconfig tests
--- e2e_tests/go.mod
+++ e2e_tests/go.mod
@@ -4,7 +4,6 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.51.0
-	cloud.google.com/go/storage v1.5.0
 	github.com/GoogleCloudPlatform/compute-image-tools/daisy v0.0.0-20200114232830-6d2d59acb179
 	github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils v0.0.0-20200114232830-6d2d59acb179
 	github.com/GoogleCloudPlatform/osconfig v0.0.0-20200115000133-a7c6fc334759
@@ -12,7 +11,6 @@ require (
 	github.com/kylelemons/godebug v1.1.0
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
 	golang.org/x/sys v0.0.0-20200113162924-86b910548bc1 // indirect
-	golang.org/x/tools v0.0.0-20200114235610-7ae403b6b589 // indirect
 	google.golang.org/api v0.15.0
 	google.golang.org/genproto v0.0.0-20200113173426-e1de0a7b01eb
 	google.golang.org/grpc v1.26.0
