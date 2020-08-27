f976352fe99118d22b7c0a8000d1ee7d60554eb5
Andrew Jackura <ajackura@google.com>
2020-08-27 10:02:40 -0700

    Make sure we retry on failed Windows Updates (#223)

--- agentendpoint/patch_windows.go
+++ agentendpoint/patch_windows.go
@@ -118,6 +118,8 @@ func (r *patchTask) wuaUpdates(ctx context.Context) error {
 		count, err := r.installWUAUpdates(ctx, cf)
 		if err != nil {
 			clog.Errorf(ctx, "Error installing Windows updates (attempt %d): %v", i, err)
+			time.Sleep(60 * time.Second)
+			continue
 		}
 		if count == 0 {
 			return nil
