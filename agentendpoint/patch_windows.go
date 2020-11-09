3e351d5668dbbcf254b191948f06fbafd88215c8
Andrew Jackura <ajackura@google.com>
2020-11-09 11:13:32 -0800

    fix

--- agentendpoint/patch_windows.go
+++ agentendpoint/patch_windows.go
@@ -118,6 +118,7 @@ func (r *patchTask) wuaUpdates(ctx context.Context) error {
 		count, err := r.installWUAUpdates(ctx, cf)
 		if err != nil {
 			clog.Errorf(ctx, "Error installing Windows updates (attempt %d): %v", i, err)
+			time.Sleep(60 * time.Second)
 			continue
 		}
 		if count == 0 {
