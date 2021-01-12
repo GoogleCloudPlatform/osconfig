f1c50f21ddc6b2f8138ad220892bd0b08fb3a22f
Andrew Jackura <ajackura@google.com>
2021-01-11 16:17:57 -0800

    Fix builds for ppc and s390x (#274)

--- packages/cos_test.go
+++ packages/cos_test.go
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+// +build linux,!s390x,!ppc64le,!ppc64
+
 package packages
 
 import (
