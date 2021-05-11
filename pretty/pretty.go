1f2f7ed42685999e3dafef984923f6a7ac85b7e5
Rafał Dowgird <dowgird@google.com>
2021-05-11 13:30:47 +0000

    Fixed comment on Format

--- pretty/pretty.go
+++ pretty/pretty.go
@@ -5,7 +5,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// PrettyFmt uses jsonpb to marshal a proto for pretty printing.
+// Format uses jsonpb to marshal a proto for pretty printing.
 func Format(pb proto.Message) string {
 	m := &protojson.MarshalOptions{Indent: "  ", AllowPartial: true, UseProtoNames: true, EmitUnpopulated: true, UseEnumNumbers: false}
 	return m.Format(pb)
