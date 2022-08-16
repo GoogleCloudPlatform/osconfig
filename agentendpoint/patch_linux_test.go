a9c6eae693bcc13edccd929692bd46f8902d4376
Aliaksei Burau <aburau@google.com>
2022-08-16 19:46:19 +0000

    Fix exclude packages field processing
    In Golang, foreach loop create only one variable per a whole loop.
    
    It is unsafe to pass pointer to the variable, cause after the loop,
    all pointers will point to last element in array.
    
    To make code safe by design, function ospatch.CreateStringExclude,
    refactored to copy pointer to a string before memorizing it.

--- agentendpoint/patch_linux_test.go
+++ agentendpoint/patch_linux_test.go
@@ -17,6 +17,7 @@ package agentendpoint
 import (
 	"reflect"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/osconfig/ospatch"
@@ -35,6 +36,7 @@ func TestExcludeConversion(t *testing.T) {
 		want  []*ospatch.Exclude
 	}{
 		{name: "StrictStringConversion", input: []string{"PackageName"}, want: []*ospatch.Exclude{ospatch.CreateStringExclude(&strictString)}},
+		{name: "MultipleStringConversion", input: []string{"PackageName1", "PackageName2"}, want: CreateStringExcludes("PackageName1", "PackageName2")},
 		{name: "RegexConversion", input: []string{"/PackageName/"}, want: []*ospatch.Exclude{ospatch.CreateRegexExclude(regex)}},
 		{name: "CornerCaseRegex", input: []string{"//"}, want: []*ospatch.Exclude{ospatch.CreateRegexExclude(emptyRegex)}},
 		{name: "CornerCaseStrictString", input: []string{"/"}, want: []*ospatch.Exclude{ospatch.CreateStringExclude(&slashString)}},
@@ -48,8 +50,27 @@ func TestExcludeConversion(t *testing.T) {
 				t.Errorf("err = %v, want %v", err, nil)
 			}
 			if !reflect.DeepEqual(excludes, tt.want) {
-				t.Errorf("convertInputToExcludes() = %v, want = %v", excludes, tt.want)
+				t.Errorf("convertInputToExcludes() = %s, want = %s", toString(excludes), toString(tt.want))
 			}
 		})
 	}
+}
+
+func toString(excludes []*ospatch.Exclude) string {
+	results := make([]string, len(excludes))
+	for i, exc := range excludes {
+		results[i] = exc.String()
+	}
+
+	return strings.Join(results, ",")
+}
+
+func CreateStringExcludes(pkgs ...string) []*ospatch.Exclude {
+	excludes := make([]*ospatch.Exclude, len(pkgs))
+	for i := 0; i < len(pkgs); i++ {
+		pkg := pkgs[i]
+		excludes[i] = ospatch.CreateStringExclude(&pkg)
+	}
+
+	return excludes
 }
