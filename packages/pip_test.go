fe9a1914e925aef75f8f0c723e249f23aa1e195f
Aliaksei Burau <aburau@google.com>
2025-05-05 12:48:31 +0000

    Write tests for pip updates

--- packages/pip_test.go
+++ packages/pip_test.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
-	"testing"
 	"reflect"
+	"testing"
 
 	utilmocks "github.com/GoogleCloudPlatform/osconfig/util/mocks"
 	utiltest "github.com/GoogleCloudPlatform/osconfig/util/utiltest"
@@ -43,7 +43,7 @@ func TestInstalledPipPackages(t *testing.T) {
 					err:    fmt.Errorf("unexpected error"),
 				},
 			},
-			expectedError:       fmt.Errorf("error running /usr/bin/pip with args [\"list\" \"--format=json\"]: unexpected error, stdout: \"\", stderr: \"\""),
+			expectedError: fmt.Errorf("error running /usr/bin/pip with args [\"list\" \"--format=json\"]: unexpected error, stdout: \"\", stderr: \"\""),
 		},
 		{
 			name: "Output from CLI is invalid JSON",
@@ -55,8 +55,7 @@ func TestInstalledPipPackages(t *testing.T) {
 					err:    nil,
 				},
 			},
-			expectedError:       fmt.Errorf("unexpected end of JSON input"),
-
+			expectedError: fmt.Errorf("unexpected end of JSON input"),
 		},
 		{
 			name: "Empty json",
@@ -69,8 +68,7 @@ func TestInstalledPipPackages(t *testing.T) {
 				},
 			},
 			expectedResults: nil,
-			expectedError:       nil,
-
+			expectedError:   nil,
 		},
 	}
 
@@ -94,6 +92,92 @@ func TestInstalledPipPackages(t *testing.T) {
 				utiltest.MatchSnapshot(t, pkgs, tt.expectedResultsFile)
 			} else if !reflect.DeepEqual(pkgs, tt.expectedResults) {
 				t.Errorf("InstalledPipPackages: unexpected result, expect %v, got %v", tt.expectedResults, pkgs)
+			}
+		})
+
+	}
+}
+
+func TestPipUpdates(t *testing.T) {
+	tests := []struct {
+		name                  string
+		expectedCommandsChain []expectedCommand
+		expectedResultsFile   string
+		expectedResults       []*PkgInfo
+		expectedError         error
+	}{
+		{
+			name: "Snapshot pip list --format=json",
+			expectedCommandsChain: []expectedCommand{
+				{
+					cmd:    exec.Command("/usr/bin/pip", "list", "--format=json", "--outdated"),
+					stdout: utiltest.BytesFromFile(t, "./testdata/linux-pip-list-outdated-format-json.stdout"),
+					stderr: []byte(""),
+					err:    nil,
+				},
+			},
+			expectedResultsFile: "./testdata/linux-pip-list-outdated-format-json.expected",
+			expectedError:       nil,
+		},
+		{
+			name: "CLI throw an error",
+			expectedCommandsChain: []expectedCommand{
+				{
+					cmd:    exec.Command("/usr/bin/pip", "list", "--format=json", "--outdated"),
+					stdout: []byte(""),
+					stderr: []byte(""),
+					err:    fmt.Errorf("unexpected error"),
+				},
+			},
+			expectedError: fmt.Errorf("error running /usr/bin/pip with args [\"list\" \"--format=json\" \"--outdated\"]: unexpected error, stdout: \"\", stderr: \"\""),
+		},
+		{
+			name: "Output from CLI is invalid JSON",
+			expectedCommandsChain: []expectedCommand{
+				{
+					cmd:    exec.Command("/usr/bin/pip", "list", "--format=json", "--outdated"),
+					stdout: utiltest.BytesFromFile(t, "./testdata/linux-pip-list-outdated-format-json.stdout")[:100],
+					stderr: []byte(""),
+					err:    nil,
+				},
+			},
+			expectedError: fmt.Errorf("unexpected end of JSON input"),
+		},
+		{
+			name: "Empty json",
+			expectedCommandsChain: []expectedCommand{
+				{
+					cmd:    exec.Command("/usr/bin/pip", "list", "--format=json", "--outdated"),
+					stdout: []byte("[]"),
+					stderr: []byte(""),
+					err:    nil,
+				},
+			},
+			expectedResults: nil,
+			expectedError:   nil,
+		},
+	}
+
+	for _, tt := range tests {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockCommandRunner := utilmocks.NewMockCommandRunner(mockCtrl)
+		runner = mockCommandRunner
+
+		ctx := context.Background()
+		t.Run(tt.name, func(t *testing.T) {
+			setExpectations(mockCommandRunner, tt.expectedCommandsChain)
+
+			pkgs, err := PipUpdates(ctx)
+			if formatError(tt.expectedError) != formatError(err) {
+				t.Errorf("PipUpdates: unexpected error, expect %q, got %q", formatError(tt.expectedError), formatError(err))
+			}
+
+			if tt.expectedResultsFile != "" {
+				utiltest.MatchSnapshot(t, pkgs, tt.expectedResultsFile)
+			} else if !reflect.DeepEqual(pkgs, tt.expectedResults) {
+				t.Errorf("PipUpdates: unexpected result, expect %v, got %v", tt.expectedResults, pkgs)
 			}
 		})
 
