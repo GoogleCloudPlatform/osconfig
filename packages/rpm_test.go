68f3596451164ea8a089dd2845baae8dd14340b6
Nikita <198777368+1pnq@users.noreply.github.com>
2025-05-07 16:35:40 +0200

    Add InstalledRPMPackages snapshot tests (#823)
    * Add InstalledRPMPackages snapshot tests
    
    * Inline matchesSnapshot helper

--- packages/rpm_test.go
+++ packages/rpm_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	utilmocks "github.com/GoogleCloudPlatform/osconfig/util/mocks"
+	utiltest "github.com/GoogleCloudPlatform/osconfig/util/utiltest"
 	"github.com/golang/mock/gomock"
 )
 
@@ -73,13 +74,14 @@ func TestInstalledRPMPackages(t *testing.T) {
 	tests := []struct {
 		name string
 
-		expectedCommandsChain []expectedCommand
-		expectedResults       []*PkgInfo
-		expectedError         error
+		cmds             []expectedCommand
+		wantPkgs         []*PkgInfo
+		wantPkgsSnapshot string
+		wantErr          error
 	}{
 		{
 			name: "success path",
-			expectedCommandsChain: []expectedCommand{
+			cmds: []expectedCommand{
 				{
 					cmd: exec.Command(rpmquery, rpmqueryInstalledArgs...),
 					stdout: []byte("" +
@@ -89,23 +91,78 @@ func TestInstalledRPMPackages(t *testing.T) {
 					err:    nil,
 				},
 			},
-			expectedResults: []*PkgInfo{
+			wantPkgs: []*PkgInfo{
 				{Name: "gcc", Arch: "x86_64", Version: "11.4.1-3.el9", Source: Source{Name: "gcc-11.4.1-3.el9.src.rpm"}},
 				{Name: "golang-src", Arch: "all", Version: "1.22.3-1.el9", Source: Source{Name: "golang-1.22.3-1.el9.src.rpm"}},
 			},
-			expectedError: nil,
+			wantErr: nil,
 		},
 		{
 			name: "rpmquery command failed",
-			expectedCommandsChain: []expectedCommand{{
+			cmds: []expectedCommand{{
 				cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
 				stdout: []byte("stdout"),
 				stderr: []byte("stderr"),
 				err:    errors.New("unexpected error"),
 			},
 			},
-			expectedResults: nil,
-			expectedError:   errors.New("error running /usr/bin/rpmquery with args [\"--queryformat\" \"\\\\{\\\"architecture\\\":\\\"%{ARCH}\\\",\\\"package\\\":\\\"%{NAME}\\\",\\\"source_name\\\":\\\"%{SOURCERPM}\\\",\\\"version\\\":\\\"%|EPOCH?{%{EPOCH}:}:{}|%{VERSION}-%{RELEASE}\\\"\\\\}\\n\" \"-a\"]: unexpected error, stdout: \"stdout\", stderr: \"stderr\""),
+			wantPkgs: nil,
+			wantErr:  errors.New("error running /usr/bin/rpmquery with args [\"--queryformat\" \"\\\\{\\\"architecture\\\":\\\"%{ARCH}\\\",\\\"package\\\":\\\"%{NAME}\\\",\\\"source_name\\\":\\\"%{SOURCERPM}\\\",\\\"version\\\":\\\"%|EPOCH?{%{EPOCH}:}:{}|%{VERSION}-%{RELEASE}\\\"\\\\}\\n\" \"-a\"]: unexpected error, stdout: \"stdout\", stderr: \"stderr\""),
+		},
+		{
+			name: "centos-7-1 mapped stdout matches snapshot",
+			cmds: []expectedCommand{
+				{
+					cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
+					stdout: utiltest.BytesFromFile(t, "./testdata/centos-7-1.rpm-query-all.stdout"),
+					stderr: []byte(""),
+				},
+			},
+			wantPkgsSnapshot: "./testdata/centos-7-1.rpm-query-all.want",
+		},
+		{
+			name: "oracle-linux-8 mapped stdout matches snapshot",
+			cmds: []expectedCommand{
+				{
+					cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
+					stdout: utiltest.BytesFromFile(t, "./testdata/oracle-linux-8.rpm-query-all.stdout"),
+					stderr: []byte(""),
+				},
+			},
+			wantPkgsSnapshot: "./testdata/oracle-linux-8.rpm-query-all.want",
+		},
+		{
+			name: "sles-12-1 mapped stdout matches snapshot",
+			cmds: []expectedCommand{
+				{
+					cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
+					stdout: utiltest.BytesFromFile(t, "./testdata/sles-12-1.rpm-query-all.stdout"),
+					stderr: []byte(""),
+				},
+			},
+			wantPkgsSnapshot: "./testdata/sles-12-1.rpm-query-all.want",
+		},
+		{
+			name: "rocky-8-1 mapped stdout matches snapshot",
+			cmds: []expectedCommand{
+				{
+					cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
+					stdout: utiltest.BytesFromFile(t, "./testdata/rocky-8-1.rpm-query-all.stdout"),
+					stderr: []byte(""),
+				},
+			},
+			wantPkgsSnapshot: "./testdata/rocky-8-1.rpm-query-all.want",
+		},
+		{
+			name: "rhel-9-1 mapped stdout matches snapshot",
+			cmds: []expectedCommand{
+				{
+					cmd:    exec.Command(rpmquery, rpmqueryInstalledArgs...),
+					stdout: utiltest.BytesFromFile(t, "./testdata/rhel-9-1.rpm-query-all.stdout"),
+					stderr: []byte(""),
+				},
+			},
+			wantPkgsSnapshot: "./testdata/rhel-9-1.rpm-query-all.want",
 		},
 	}
 
@@ -117,15 +174,17 @@ func TestInstalledRPMPackages(t *testing.T) {
 		runner = mockCommandRunner
 
 		t.Run(tt.name, func(t *testing.T) {
-			setExpectations(mockCommandRunner, tt.expectedCommandsChain)
+			setExpectations(mockCommandRunner, tt.cmds)
 
-			results, err := InstalledRPMPackages(testCtx)
-			if !reflect.DeepEqual(err, tt.expectedError) {
-				t.Errorf("InstalledRPMPackages: unexpected error, expect %q, got %q", formatError(tt.expectedError), formatError(err))
+			pkgs, err := InstalledRPMPackages(testCtx)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("InstalledRPMPackages err: want %v, got %v", tt.wantErr, err)
 			}
 
-			if !reflect.DeepEqual(results, tt.expectedResults) {
-				t.Errorf("InstalledRPMPackages: unexpected result, expect %v, got %v", pkgs, tt.expectedResults)
+			if tt.wantPkgsSnapshot != "" {
+				utiltest.MatchSnapshot(t, pkgs, tt.wantPkgsSnapshot)
+			} else if !reflect.DeepEqual(pkgs, tt.wantPkgs) {
+				t.Errorf("InstalledRPMPackages pkgs: want %v, got %v", tt.wantPkgs, pkgs)
 			}
 		})
 	}
