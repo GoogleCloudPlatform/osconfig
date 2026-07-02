71514b251da18a4a906468be926971d2ab43c58f
Dmitrii Ganochenko <dmitriig@google.com>
2026-07-02 02:12:44 +0200

    add tests

--- pretty/pretty_test.go
+++ pretty/pretty_test.go
@@ -1,30 +1,92 @@
 package pretty
 
 import (
+	"encoding/json"
 	"testing"
 
 	"cloud.google.com/go/osconfig/agentendpoint/apiv1/agentendpointpb"
+	"github.com/GoogleCloudPlatform/osconfig/util/utiltest"
 )
 
-func TestFormat(t *testing.T) {
-	//flaky test
-	t.Skip()
+// TestMarshalOptions tests the MarshalOptions function.
+func TestMarshalOptions(t *testing.T) {
+	opts := MarshalOptions()
 
-	input := &agentendpointpb.Inventory_OsInfo{
-		Hostname:             "Hostname",
-		LongName:             "LongName",
-		ShortName:            "ShortName",
-		Version:              "Version",
-		Architecture:         "Architecture",
-		KernelVersion:        "KernelVersion",
-		KernelRelease:        "KernelRelease",
-		OsconfigAgentVersion: "OSConfigAgentVersion",
+	utiltest.AssertEquals(t, opts.Indent, "  ")
+	utiltest.AssertEquals(t, opts.AllowPartial, true)
+	utiltest.AssertEquals(t, opts.UseProtoNames, true)
+	utiltest.AssertEquals(t, opts.EmitUnpopulated, true)
+	utiltest.AssertEquals(t, opts.UseEnumNumbers, false)
+}
+
+// TestFormat tests the Format function.
+func TestFormat(t *testing.T) {
+	var dummyMap map[string]any
+	jsonUnmarshalErr := json.Unmarshal([]byte("<nil>"), &dummyMap)
+	wantFormat := `(?s)^\{.+\}$`
+	var gotMap map[string]any
+
+	tests := []struct {
+		name    string
+		input   *agentendpointpb.Inventory_OsInfo
+		wantMap map[string]any
+		wantErr error
+	}{
+		{
+			name: "populated fields, want formatted JSON with matching values",
+			input: &agentendpointpb.Inventory_OsInfo{
+				Hostname:             "Hostname",
+				LongName:             "LongName",
+				ShortName:            "ShortName",
+				Version:              "Version",
+				Architecture:         "Architecture",
+				KernelVersion:        "KernelVersion",
+				KernelRelease:        "KernelRelease",
+				OsconfigAgentVersion: "OSConfigAgentVersion",
+			},
+			wantMap: map[string]any{
+				"hostname":               "Hostname",
+				"long_name":              "LongName",
+				"short_name":             "ShortName",
+				"version":                "Version",
+				"architecture":           "Architecture",
+				"kernel_version":         "KernelVersion",
+				"kernel_release":         "KernelRelease",
+				"osconfig_agent_version": "OSConfigAgentVersion",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "empty message, want formatted JSON with unpopulated fields",
+			input: &agentendpointpb.Inventory_OsInfo{},
+			wantMap: map[string]any{
+				"hostname":               "",
+				"long_name":              "",
+				"short_name":             "",
+				"version":                "",
+				"architecture":           "",
+				"kernel_version":         "",
+				"kernel_release":         "",
+				"osconfig_agent_version": "",
+			},
+			wantErr: nil,
+		},
+		{
+			name:    "nil input, want empty output and json unmarshal error",
+			input:   nil,
+			wantMap: nil,
+			wantErr: jsonUnmarshalErr,
+		},
 	}
 
-	expected := "{\n  \"hostname\":  \"Hostname\",\n  \"long_name\":  \"LongName\",\n  \"short_name\":  \"ShortName\",\n  \"version\":  \"Version\",\n  \"architecture\":  \"Architecture\",\n  \"kernel_version\":  \"KernelVersion\",\n  \"kernel_release\":  \"KernelRelease\",\n  \"osconfig_agent_version\":  \"OSConfigAgentVersion\"\n}"
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			gotStr := Format(tc.input)
+			gotErr := json.Unmarshal([]byte(gotStr), &gotMap)
 
-	output := Format(input)
-	if expected != output {
-		t.Errorf("unexpected output\nexpected:\n%q\ngot:\n%q", expected, output)
+			utiltest.AssertErrorMatchAndSkip(t, gotErr, tc.wantErr)
+			utiltest.AssertFormatMatch(t, gotStr, wantFormat)
+			utiltest.AssertEquals(t, gotMap, tc.wantMap)
+		})
 	}
 }
